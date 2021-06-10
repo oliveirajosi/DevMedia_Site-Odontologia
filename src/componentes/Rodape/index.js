d5ebd4f74d04186483d25af391bf153b02b6ebc8
Josiane Oliveira <josianeoliveirati@gmail.com>
2021-06-10 13:13:57 -0300

    Mudanças no rodapé

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -5,7 +5,7 @@ const Rodape = () => {
   return (
     <footer className="rodape">
       <div className="limitar-container">
-        © Copyright 2021   |   Consultório dentes saudáveis
+        © Copyright 2021   |   Consultório Dentes Saudáveis
       </div>
     </footer>
   );
